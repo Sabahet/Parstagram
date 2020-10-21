dc39e3e2dc56f83d68fe625190da8bee4da94de4
Sabahet Alovic <61169304+Sabahet@users.noreply.github.com>
2020-10-20 21:41:21 -0400

    Add files via upload

---
+++ PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  Parstagram2
+//
+//  Created by Sabahet Alovic on 10/20/20.
+//  Copyright © 2020 Sabahet Alovic. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram2
+//
+//  Created by Sabahet Alovic on 10/19/20.
+//  Copyright © 2020 Sabahet Alovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
